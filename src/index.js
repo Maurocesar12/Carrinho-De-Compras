f45c9d45ce3a6ff3faf9cfabd83087f63ebf83c9
Mauro César <mauroguima080@hotmail.com>
2025-04-01 12:38:49 -0300

    Finalizando o estudo do projeto

--- src/index.js
+++ src/index.js
@@ -3,15 +3,23 @@ import * as cartService from './services/cart.js';
 import createItem from './services/item.js';
 
 const myCart = [];
-const myWhihList = [];
+const myWhishList = [];
 
 console.log("BEM-VINDO AO CARRINHO!");
 
-const item1 = await createItem("Teclado", 20.33, 2);
+const item1 = await createItem("Teclado", 20.33, 1);
 const item2 = await createItem("Mouse", 59.99, 2);
 
 await cartService.addItem(myCart, item1);
-await cartService.addItem(myWhihList, item2);
+await cartService.addItem(myCart, item2);
+
+await cartService.removeItem(myCart, 1);
+
+await cartService.displayCart(myCart);
+
+
+//await cartService.deleteItem(myCart, item2.name);
+//await cartService.deleteItem(myCart, item1.name);
 
 console.log("Total Do Carrinho:");
 
--- src/services/cart.js
+++ src/services/cart.js
@@ -14,19 +14,37 @@ async function calculateTotal(userCart) {
 
 //Deletar item do carrinho
 async function deleteItem(userCart, name) {
+    const index = userCart.findIndex((item) => item.name === name);
 
+    if(index !== -1) {
+        userCart.splice(index, 1);
+    }
 };
 
 //Remove um item do carrinho
 async function removeItem(userCart, index) {
-    
+    //transforma o item visual do usuario, para o index do backend
+    const deleteIndex = index - 1;
+
+    //Verifico se o index é menor que o zero
+    if(index >= 0 && index < userCart.length) {
+        userCart.splice(deleteIndex, 1);
+    }
 };
+
+async function displayCart(userCart) {
+    console.log("Carriho cart list:");
+    userCart.forEach((item, index) => {
+        console.log(`${index + 1}. ${item.name} - R$ ${item.price} / ${item.quantify}X / Subtotal ${item.subtotal()}`);
+    });
+}
 
 export {
     addItem,
     deleteItem,
     removeItem,
     calculateTotal,
+    displayCart,
 };
 
 
